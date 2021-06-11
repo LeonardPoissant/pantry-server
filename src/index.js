0c68cd0b3f668a4f5183fdf9e1c2151f09055b6b
Leonard Poissant <leonardtati@gmail.com>
2021-06-11 14:47:42 -0400

    scripts

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ app.post('/register', register)
 
 var MongoClient = require('mongodb').MongoClient;
 var db;
-let connectionString = process.env.MONGO_URL;
+let connectionString = process.env.MONGO_URI;
 
 // Initialize connection once
 MongoClient.connect(connectionString, {
