f02ac60f1335280c6fbc5387dacda352b2864726
Leonard <leonardtati@gmail.com>
2022-03-07 19:38:22 -0500

    more test

--- src/auth.js
+++ src/auth.js
@@ -7,12 +7,13 @@ const verifyToken = (req, res, next) => {
     req.body.token || req.query.token || req.headers["x-access-token"];
 
     console.log('in here',token)
+    const privateKey = "iU6EOY2RlO9SDxP7KBPO/j/wN8TXKCalMQr1OOhyjFE"
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, config.secretKey);
+    const decoded = jwt.verify(token, privateKey);
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
